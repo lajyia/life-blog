2f39fa6af9b788787daa771308063b7772ca2dc4
stpnxx <99842216+stpnxx@users.noreply.github.com>
2023-02-09 14:15:17 +0300

    add validation to update and change profile

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -1,64 +1,80 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
-const {validationResult} = require('express-validator');
+const { validationResult } = require('express-validator');
 
 class ProfileController {
     async getProfile(req, res) {
-        const {nickname} = req.body;
-        const candidate = await User.findOne({nickname});
-        if (candidate){
-            return res.json({candidate});
+        const { nickname } = req.body;
+        const errors = validationResult(req).errors;
+        if (errors.length > 0) {
+            return res.status(400).json({ errors });
         }
-        return res.status(400).json({message: "A user with this nickname was not found"})
+        const candidate = await User.findOne({ nickname });
+        if (candidate) {
+            return res.json({ candidate });
+        }
+        return res.status(400).json({ message: "A user with this nickname was not found" })
     }
 
     async updateProfile(req, res) {
-        const {linkName, newLinkName, bio} = req.body;
-        const linkCandidate = await User.findOne({linkName});
-        if (linkCandidate){
-            await User.findOneAndUpdate({linkName}, {bio, linkName: newLinkName}, {new: true});
-            return res.json({message: "A profile has been updated"})
+        const { linkName, newLinkName, bio } = req.body;
+        const errors = validationResult(req).errors;
+        if (errors.length > 0) {
+            return res.status(400).json({ errors });
         }
-        return res.status(400).json({message: "A user with this linkName was not found"})
+        const linkCandidate = await User.findOne({ linkName });
+        if (linkCandidate) {
+            await User.findOneAndUpdate({ linkName }, { bio, linkName: newLinkName }, { new: true });
+            return res.json({ message: "A profile has been updated" })
+        }
+        return res.status(400).json({ message: "A user with this linkName was not found" })
     }
 
-    async changeProfile(req, res){
-        const {nickname, newNickname, password} = req.body;
-        const candidate = User.findOne({nickname});
-        if (candidate){
-            const hashPassword = bcrypt.hashSync(password, 7);
-            console.log(hashPassword);
-            await User.findOneAndUpdate({nickname}, {nickname: newNickname, password: hashPassword}, {new: true})
+    async changeProfile(req, res) {
+        const { nickname, newNickname, password } = req.body;
+        const errors = validationResult(req).errors;
+        if (errors.length > 0) {
+            return res.status(400).json({ errors });
         }
-        return res.status(400).json({message: 'A user with this nickame was not found'})
+        const candidate = User.findOne({ nickname });
+        if (candidate) {
+            const hashPassword = bcrypt.hashSync(password, 7);
+            await User.findOneAndUpdate({ nickname }, { nickname: newNickname, password: hashPassword }, { new: true });
+            return res.json({ message: 'A user has been changed' })
+        }
+        return res.status(400).json({ message: 'A user with this nickame was not found' })
     }
 
     async deleteProfile(req, res) {
-        const {nickname} = req.body;
-        const candidate = await User.findOne({nickname});
-        if (candidate){
-            await User.deleteOne({nickname});
-            return res.json({message: "A user has been deleted"})
+        const { nickname } = req.body;
+        const errors = validationResult(req).errors;
+        if (errors.length > 0) {
+            return res.status(400).json({ errors });
         }
-        return res.status(400).json({message: "A user with this nickname was not found"})
+        const candidate = await User.findOne({ nickname });
+        if (candidate) {
+            await User.deleteOne({ nickname });
+            return res.json({ message: "A user has been deleted" })
+        }
+        return res.status(400).json({ message: "A user with this nickname was not found" })
     }
 
-    async loginProfile(req, res){
-        const {nickname, password} = req.body;
+    async loginProfile(req, res) {
+        const { nickname, password } = req.body;
         const errors = validationResult(req).errors;
-        if (errors.length > 0){
-            return res.status(400).json({errors});
+        if (errors.length > 0) {
+            return res.status(400).json({ errors });
         }
-        const candidate = await User.findOne({nickname});
-        if (candidate){
+        const candidate = await User.findOne({ nickname });
+        if (candidate) {
             const passwordUser = candidate.password;
             const isLogin = bcrypt.compareSync(password, passwordUser);
-            if (isLogin){
-                return res.json({candidate})
+            if (isLogin) {
+                return res.json({ candidate })
             }
-            return res.status(400).json({message: 'Invalid password'})
+            return res.status(400).json({ message: 'Invalid password' })
         }
-        return res.status(400).json({message: "No user with this name"})
+        return res.status(400).json({ message: "No user with this name" })
     }
 }
 
--- server/routers/profileRouter.js
+++ server/routers/profileRouter.js
@@ -3,13 +3,30 @@ const router = new Router();
 const controller = require('../controllers/profileController');
 const {check} = require('express-validator');
 
-router.post('/', controller.getProfile);
-router.post('/delete', controller.deleteProfile);
-router.post('/update', controller.updateProfile);
+router.post('/', [
+    check("nickname", "Nickname can't be smaller 5 lettes and more 10").isLength({min: 5, max: 10})
+], controller.getProfile);
+
+router.post('/delete', [
+    check("nickname", "Nickname can't be smaller 5 lettes and more 10").isLength({min: 5, max: 10})
+], controller.deleteProfile);
+
+router.post('/update', [
+    check("linkName", "LinkName can't be smaller 5 lettes and more 10").isLength({min: 5, max: 10}),
+    check("newLinkName", "LinkName can't be smaller 5 lettes and more 10").isLength({min: 5, max: 10}),
+    check("bio", "Bio can't be more 40 letters").isLength({max: 10}),
+], controller.updateProfile);
+
 router.post('/login', [
     check("nickname","Nickname can't be smaller 5 lettes and more 10").isLength({min: 5, max: 10}),
     check("password", "Password can't be smaller 8 letters").isLength({min: 8})
 ], controller.loginProfile);
-router.post('/change', controller.changeProfile);
+
+router.post('/change', [
+    check("nickname", "Nickname can't be smaller 5 lettes and more 10").isLength({min: 5, max: 10}),
+    check("newNickname", "Nickname can't be smaller 5 lettes and more 10").isLength({min: 5, max: 10}),
+    check("password", "Password can't be smaller 8 lettes").isLength({min: 8}),
+],
+controller.changeProfile);
 
 module.exports = router;
