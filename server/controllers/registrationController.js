5b654fef34eedb780d8320c79e34d499e60f74bc
stpnxx <99842216+stpnxx@users.noreply.github.com>
2023-02-09 12:27:20 +0300

    add validation to password

--- server/controllers/registrationController.js
+++ server/controllers/registrationController.js
@@ -1,9 +1,15 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const {validationResult} = require('express-validator');
+
 
 class RegistrationController {
     async addUser(req, res){
         const {nickname, linkName, password} = req.body;
+        const errors = validationResult(req).errors;
+        if (errors.length > 0){
+            return res.status(400).json({errors})
+        }
         const candidateName = await User.findOne({nickname});
         const candidateLink = await User.findOne({linkName});
         if (!candidateLink && !candidateName){
--- server/routers/registrationRouter.js
+++ server/routers/registrationRouter.js
@@ -1,9 +1,14 @@
 const Router = require('express');
 const router = new Router();
 const controller = require('../controllers/registrationController');
+const {check} = require('express-validator');
 
 
-router.post('/', controller.addUser );
+router.post('/',[
+    check("password", "Password can't be smaller 8 letters").isLength({min: 8}),
+    check("nickname", "Nickname can't be smaller 5 lettes and more 10").isLength({min: 5, max: 10}),
+    check("linkName", "Link cant'be smaller 5 letters amd more 10").isLength({min: 5, max: 10}),
+], controller.addUser );
 
 
 module.exports = router;
