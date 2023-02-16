9a8b8275a09fb5ac2674442b0d64fec80fcb9ced
= <senthiedo@gmail.com>
2023-02-16 23:40:20 +0300

    add validation password to form

--- client/src/pages/Registration.tsx
+++ client/src/pages/Registration.tsx
@@ -1,13 +1,33 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import '../styles/Registration.css';
 import Button from '../components/UI/Button/Button';
-import Input from '../components/UI/Input/Input';
 import FormImage from '../images/form-image.png';
 import UserForm from '../images/user-form.svg';
 import LockForm from '../images/lock-form.svg';
 import SlashForm from '../images/slash-form.svg'
 
+
+interface validForm{
+    password: boolean,
+    linkname: boolean,
+    login: boolean
+}
+
+
 const Registration: FC = () => {
+
+
+    const [validForm, setValidForm] = useState<validForm>({password: false, linkname: false, login: false});
+
+    const changePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+        const length: number = e.target.value.length;
+
+        if (length > 7) {
+            setValidForm({...validForm, password: true})
+        }
+    }
+
     return (
         <div className='registration'>
             <div className="registration__container">
@@ -21,32 +41,35 @@ const Registration: FC = () => {
                             <div className="registration__login">
                                 <div className="registration__input">
                                     <img src={UserForm} alt="" />
-                                    <Input type="text" placeholder="Login" />
+                                    <input type="text" placeholder="Login" />
                                 </div>
                                 <div className="registration__login-error error-form">aaa</div>
                             </div>
                             <div className="registration__password">
                                 <div className="registration__input">
                                     <img src={LockForm} alt="" />
-                                    <Input placeholder="Password" type="password" />
+                                    <input onChange={changePassword} placeholder="Password" type="password" />
                                 </div>
-                                <div className="registration__password-error error-form">password cant'be smaller 8 letters</div>
-                            </div>
-                            <div className="registration__linkname">
-                                <div className="registration__input">
-                                    <img className='registration__image-slash' src={SlashForm} alt="" />
-                                    <Input placeholder="Linkname" type="linkname" />
-                                </div>
-                                <div className="registration__linkname-error error-form">aaa</div>
-                            </div>
-                            <Button>send</Button>
-                        </form>
+                                {!validForm.password
+                                    ? <div className="registration__password-error error-form">password cant'be smaller 8 letters</div>
+                                    : <div></div>
+                                }
                     </div>
-
-                </div>
-
+                    <div className="registration__linkname">
+                        <div className="registration__input">
+                            <img className='registration__image-slash' src={SlashForm} alt="" />
+                            <input placeholder="Linkname" type="linkname" />
+                        </div>
+                        <div className="registration__linkname-error error-form">aaa</div>
+                    </div>
+                    <Button>send</Button>
+                </form>
             </div>
+
         </div>
+
+            </div >
+        </div >
     );
 };
 
