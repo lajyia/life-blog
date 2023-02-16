af8c546218cb8158456d3c0432d82de92b55000d
= <senthiedo@gmail.com>
2023-02-17 00:03:51 +0300

    add validaion to all form;s input

--- client/src/pages/Registration.tsx
+++ client/src/pages/Registration.tsx
@@ -7,7 +7,7 @@ import LockForm from '../images/lock-form.svg';
 import SlashForm from '../images/slash-form.svg'
 
 
-interface validForm{
+interface validForm {
     password: boolean,
     linkname: boolean,
     login: boolean
@@ -17,16 +17,43 @@ interface validForm{
 const Registration: FC = () => {
 
 
-    const [validForm, setValidForm] = useState<validForm>({password: false, linkname: false, login: false});
+    const [validForm, setValidForm] = useState<validForm>({ password: true, linkname: true, login: true });
 
     const changePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
 
         const length: number = e.target.value.length;
 
         if (length > 7) {
-            setValidForm({...validForm, password: true})
+            setValidForm({ ...validForm, password: true })
+        }
+        else {
+            setValidForm({ ...validForm, password: false })
         }
     }
+
+
+    const changeLogin = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const length = e.target.value.length;
+
+        if (length > 4) {
+            setValidForm({ ...validForm, login: true })
+        }
+        else {
+            setValidForm({ ...validForm, login: false })
+        }
+    }
+
+    const changeLinkname = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const length = e.target.value.length;
+
+        if (length > 4) {
+            setValidForm({ ...validForm, linkname: true })
+        }
+        else {
+            setValidForm({ ...validForm, linkname: false })
+        }
+    }
+
 
     return (
         <div className='registration'>
@@ -41,9 +68,12 @@ const Registration: FC = () => {
                             <div className="registration__login">
                                 <div className="registration__input">
                                     <img src={UserForm} alt="" />
-                                    <input type="text" placeholder="Login" />
+                                    <input onChange={changeLogin} type="text" placeholder="Login" />
                                 </div>
-                                <div className="registration__login-error error-form">aaa</div>
+                                {!validForm.login
+                                    ? <div className="registration__login-error error-form">linkname can't be smaller 5 letters</div>
+                                    : <div></div>
+                                }
                             </div>
                             <div className="registration__password">
                                 <div className="registration__input">
@@ -54,13 +84,16 @@ const Registration: FC = () => {
                                     ? <div className="registration__password-error error-form">password cant'be smaller 8 letters</div>
                                     : <div></div>
                                 }
-                    </div>
-                    <div className="registration__linkname">
-                        <div className="registration__input">
-                            <img className='registration__image-slash' src={SlashForm} alt="" />
-                            <input placeholder="Linkname" type="linkname" />
-                        </div>
-                        <div className="registration__linkname-error error-form">aaa</div>
+                            </div>
+                            <div className="registration__linkname">
+                                <div className="registration__input">
+                                    <img className='registration__image-slash' src={SlashForm} alt="" />
+                                    <input onChange={changeLinkname} placeholder="Linkname" type="linkname" />
+                                </div>
+                                {!validForm.linkname
+                                    ? <div className="registration__linkname-error error-form">linkname cant'be smaller 5 letters</div>
+                                    : <div></div>
+                                }
                     </div>
                     <Button>send</Button>
                 </form>
