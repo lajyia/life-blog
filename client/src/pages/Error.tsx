539fadf0b863c6f0ea1fad4a8c7f2b7f12ed385f
= <senthiedo@gmail.com>
2023-02-19 11:43:35 +0300

    add error page

--- client/src/pages/Error.tsx
+++ client/src/pages/Error.tsx
@@ -1,9 +1,23 @@
 import React, { FC } from 'react';
+import PigNose from '../images/pig-nose.svg';
+import '../styles/Error.css';
+import Header from '../components/Header';
 
 const Error: FC = () => {
     return (
-        <div>
-            Error!
+        <div className='error'>
+            <Header />
+            <div className="error__container">
+                <div className="error__body">
+                    <div className="error__image">
+                        <img src={PigNose} alt="" />
+                    </div>
+                    <div className="error__text-body">
+                        <div className="error__title">Page Not Found</div>
+                        <div className="error__text">Error 404</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
